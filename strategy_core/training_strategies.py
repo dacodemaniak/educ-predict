ff69ca8bb3ff4759039e4956dedcb2bcc4a2415b
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2025-12-18 11:43:20 +0100

    UPDATE (artifacts) Add artifacts generation and storage

--- strategy_core/training_strategies.py
+++ strategy_core/training_strategies.py
@@ -1,9 +1,11 @@
 from abc import ABC, abstractmethod
 import pandas as pd
 import mlflow
+import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import root_mean_squared_error, r2_score, accuracy_score, f1_score
+from sklearn.metrics import PrecisionRecallDisplay, RocCurveDisplay, auc, roc_curve, root_mean_squared_error, r2_score, accuracy_score, f1_score, confusion_matrix
 
 from loguru import logger
 
@@ -31,6 +33,49 @@ class TrainingStrategy(ABC):
         else:
             mlflow.log_metric("accuracy", accuracy_score(y_true=y_true, y_pred=y_pred))
             mlflow.log_metric("f1_score", f1_score(y_true=y_true, y_pred=y_pred))
+
+    def save_artifacts(self, model, X, y, y_pred, scenario_name):
+        # Confusion matrix
+        plt.figure(figsize=(8,6))
+        cm = confusion_matrix(y, y_pred)
+        sns.heatmap(cm, annot=True, fmt="d", cmap="Blues",
+                    xticklabels=["Success", "Failed"],
+                    yticklabels=["Success", "Failed"]
+        )
+        plt.title(f"Confusion Matrix {scenario_name}")
+        plt.ylabel('Real')
+        plt.xlabel('Predicted')
+        cm_path = f"confusion_matrix_{scenario_name}.png"
+        plt.savefig(cm_path)
+        mlflow.log_artifact(cm_path)
+        plt.close()
+
+        # Correlation matrix
+        plt.figure(figsize=(12,10))
+        corr = X.corr()
+        sns.heatmap(corr, annot=False, cmap="coolwarm")
+        plt.title(f"Features correlation - {scenario_name}")
+        corr_path = f"correlation_{scenario_name}.png"
+        plt.savefig(corr_path)
+        mlflow.log_artifact(corr_path)
+        plt.close()
+
+        # ROC curve : Receiver Operating Characteristic
+        # Use AUC metric (Area Under the Curve)
+        plt.figure(figsize=(8,6))
+        # Class 1 (failed) probality
+        y_prob = model.predict_proba(X)[:, 1] if hasattr(model, "predict_proba") else y_pred
+
+        RocCurveDisplay.from_predictions(y, y_prob, name=scenario_name)
+        plt.plot([0,1], [0,1], "k--", label="Chance (AUC=0.5)")
+        plt.title(f"ROC curve - {scenario_name}")
+        roc_path = f"roc_curve_{scenario_name}.png"
+        plt.savefig(roc_path)
+        mlflow.log_artifact(roc_path)
+
+        # Log AUC in metrics
+        fpr, tpr, _ = roc_curve(y, y_prob)
+        mlflow.log_metric("auc_score", auc(fpr, tpr))
 
 ## ==========================================================================================
 # Concrete strategies
@@ -63,6 +108,16 @@ class LogisticRegressionStrategy(TrainingStrategy):
             self.log_metrics(y, y_pred, metrics_type="classification")
             mlflow.sklearn.log_model(model, "model")
             
+            # Specific artifact Recall
+            pr_display = PrecisionRecallDisplay.from_estimator(model, X, y)
+            pr_display.figure_.suptitle(f"PR Curve - {scenario_name}")
+            plt.savefig("pr_curve.png")
+            mlflow.log_artifact("pr_curve.png")
+            plt.close()
+
+            # Common artifacts
+            self.save_artifacts(model, X, y, y_pred, scenario_name)
+
             logger.info(f"🏆 Scenario {self.scenario_id} (LR) done.")
 
 
