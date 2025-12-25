1a125d5288ab405b41be0ef6195c0f694da10f3f
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2025-12-25 03:38:12 +0100

    FIX (pipeline) Fix dynamic imports after refactorage

--- core/pipeline_core/pipeline_builder.py
+++ core/pipeline_core/pipeline_builder.py
@@ -29,11 +29,11 @@ class PipelineBuilder:
                 # Looking for strategy in the Notebook or imported files
                 strategy = PipelineBuilder._get_class_from_anywhere(
                     strat_name, 
-                    "strategy_core.training_strategies"
+                    "core.strategy_core.training_strategies"
                 )(scenario_id=scenario['label'], exclusions=scenario['exclusions'])
                 
                 # ModelHandler is supposed to be defined in the Notebook or pipeline_core
-                ModelHandlerClass = PipelineBuilder._get_class_from_anywhere("ModelHandler")
+                ModelHandlerClass = PipelineBuilder._get_class_from_anywhere("ModelHandler", "core.handlers.model_handler")
                 handler = ModelHandlerClass(strategy=strategy, scenario_label=scenario['label'])
                 orchestrator.add_handler(handler)
 
@@ -49,10 +49,11 @@ class PipelineBuilder:
         
         # 2. If not found and a module path is provided, import the .py file
         if module_path:
+            logger.debug(f"🔍 Looking for class '{class_name}' in module '{module_path}'")
             module = importlib.import_module(module_path)
             return getattr(module, class_name)
         
-        raise AttributeError(f"❌ Class '{class_name}' is not found in the Notebook or the module.")
+        raise AttributeError(f"❌ Class '{class_name}' is not found in the Notebook or the module {module_path}.")
 
     @staticmethod
     def _instantiate_notebook_handler(name, config, params):
@@ -64,21 +65,28 @@ class PipelineBuilder:
             strategy_params = {k: v for k, v in params.items() if k != 'target_columns'}
             StrategyClass = PipelineBuilder._get_class_from_anywhere(
                 config['strategy'], 
-                "strategy_core.outliers_strategies"
+                "core.strategy_core.outliers_strategies"
             )
             strat = StrategyClass(**strategy_params)
-            HandlerClass = PipelineBuilder._get_class_from_anywhere("OutlierHandler")
+            HandlerClass = PipelineBuilder._get_class_from_anywhere("OutlierHandler", "core.handlers.outlier_handler")
             return HandlerClass(strategy=strat, target_columns=target_cols)
         
         if name == "ImputationHandler":
             StrategyClass = PipelineBuilder._get_class_from_anywhere(
                 config['strategy'], 
-                "strategy_core.imputation_strategies"
+                "core.strategy_core.imputation_strategies"
             )
             strat = StrategyClass(**params)
-            HandlerClass = PipelineBuilder._get_class_from_anywhere("ImputationHandler")
+            HandlerClass = PipelineBuilder._get_class_from_anywhere("ImputationHandler", "core.handlers.imputation_handler")
             return HandlerClass(strategy=strat)
 
         # General case: retrieve the class from the Notebook and instantiate it
-        HandlerClass = PipelineBuilder._get_class_from_anywhere(name)
+        HandlerClass = PipelineBuilder._get_class_from_anywhere(class_name=name, module_path="core.handlers."+PipelineBuilder._to_snake_case(name))
         return HandlerClass(**params)
+    
+    @staticmethod
+    def _to_snake_case(name: str) -> str:
+        import re
+        partial_s = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+        snake_s = re.sub('([a-z0-9])([A-Z])', r'\1_\2', partial_s)
+        return snake_s.lower()
