6741f9c9a5f4ead85d7baa3ddc2b3fff0258dab6
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2025-12-25 03:07:42 +0100

    UPDATE (learning) Add shuffle methode to mix dataframe before training

--- core/handlers/model_handler.py
+++ core/handlers/model_handler.py
@@ -1,5 +1,8 @@
+from typing import cast
 from core.pipeline_core.pipeline_core import DataHandler, PipelineContext
 from core.strategy_core.training_strategies import TrainingStrategy
+from sklearn.utils import shuffle
+from pandas import DataFrame
 from loguru import logger
 
 class ModelHandler(DataHandler):
@@ -21,9 +24,10 @@ class ModelHandler(DataHandler):
             raise ValueError("Dataframe is none or empty. Training was interrupted")
         
         working_df = context.final_df.copy()
+        shuffled_df = cast(DataFrame, shuffle(working_df, random_state=42))
 
         try:
-            self.strategy.execute(working_df, self.scenario_label)
+            self.strategy.execute(shuffled_df, self.scenario_label)
             context.logs[f"model_{self.scenario_label}"] = "Success"
             
         except Exception as e:
