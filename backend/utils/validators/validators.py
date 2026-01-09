1222162d65c503550c17116ef17b420f3fffa6d5
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2026-01-09 08:12:48 +0100

    UPDATE (api model) Use Literal instead of poor str, refactor data model to improve reading

--- backend/utils/validators/validators.py
+++ backend/utils/validators/validators.py
@@ -1,19 +1,51 @@
 from datetime import datetime
 from pydantic import BaseModel, Field
-from typing import List, Dict, Any, Optional
+from typing import List, Dict, Any, Literal, Optional
 
 class StudentInput(BaseModel):
-    school: str = Field(..., example="GP")
-    sex: str = Field(..., example="F")
+    # --- Demographical informations ---
+    school: Literal["GP", "MS"] = Field(..., description="Student school")
+    sex: Literal["F", "M"] = Field(..., description="Student gender")
     age: int = Field(..., ge=15, le=22)
+    address: Literal["U", "R"] = Field(..., description="U: Urban, R: Country")
+    famsize: Literal["GT3", "LE3"] = Field(..., description="Taille de la famille")
+    Pstatus: Literal["T", "A"] = Field(..., description="Parents living habits")
+
+    # --- Parents works and degree ---
+    Medu: int = Field(..., ge=0, le=4, description="Mother degree")
+    Fedu: int = Field(..., ge=0, le=4, description="Father degree")
+    Mjob: Literal["teacher", "health", "services", "at_home", "other"]
+    Fjob: Literal["teacher", "health", "services", "at_home", "other"]
+    reason: Literal["home", "reputation", "course", "other"]
+    guardian: Literal["mother", "father", "other"]
+
+    # --- Student life habits ---
+    traveltime: int = Field(..., ge=1, le=4)
     studytime: int = Field(..., ge=1, le=4)
-    failures: int = Field(..., ge=0)
-    absences: int = Field(..., ge=0)
+    failures: int = Field(..., ge=0, le=4)
+    schoolsup: Literal["yes", "no"]
+    famsup: Literal["yes", "no"]
+    paid: Literal["yes", "no"]
+    activities: Literal["yes", "no"]
+    nursery: Literal["yes", "no"]
+    higher: Literal["yes", "no"]
+    internet: Literal["yes", "no"]
+    romantic: Literal["yes", "no"]
+
+    # --- Leasure and relationships (Scale 1..5) ---
+    famrel: int = Field(..., ge=1, le=5)
+    freetime: int = Field(..., ge=1, le=5)
+    goout: int = Field(..., ge=1, le=5)
+    Dalc: int = Field(..., ge=1, le=5, description="Week alcohol habits")
+    Walc: int = Field(..., ge=1, le=5, description="Weekend alchol habits")
+    health: int = Field(..., ge=1, le=5)
+    
+    # --- Results and absences ---
+    absences: int = Field(..., ge=0, le=93)
     G1: int = Field(..., ge=0, le=20)
     G2: int = Field(..., ge=0, le=20)
 
 class StepParams(BaseModel):
-    # Flexible pour accepter les params de DataLoader, Outliers, etc.
     files_to_load: Optional[Dict[str, str]] = None
     sep: Optional[str] = ";"
     contamination: Optional[float] = 0.01
