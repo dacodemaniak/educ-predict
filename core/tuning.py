292833fe915eb8e2e40de01083c2edb464217224
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2026-01-13 14:22:31 +0100

    ADD (hyper parameters) Update and build best model on deployment

--- core/tuning.py
+++ core/tuning.py
@@ -1,3 +1,4 @@
+import sys
 import optuna
 import yaml
 import shutil
@@ -10,37 +11,43 @@ from prefect import flow, task
 
 
 @task(name="Optuna_Search")
-def optimize_hyperparams():
+def optimize_hyperparams(algo: str = "RF"):
     def objective(trial):
-        # Sets search space
-        params = {
-            "n_estimators": trial.suggest_int("n_estimators", 50, 200),
-            "max_depth": trial.suggest_int("max_depth", 5, 30),
-            "min_samples_split": trial.suggest_int("min_samples_split", 2, 10)
-        }
-
         # ... Training (optimization mode) ...
         logger.info("🚀 Training pipeline starting (CI/CD Mode)...")
         orchestrator = PipelineOrchestrator()
         context = PipelineContext()
-        # Add params in the context
-        context.add_variable("temp_rf_params", params)
+        
+        if algo == "RF":
+            params = {
+                "n_estimators": trial.suggest_int("n_estimators", 50, 200),
+                "max_depth": trial.suggest_int("max_depth", 5, 30),
+                "min_samples_split": trial.suggest_int("min_samples_split", 2, 10)
+            }
+            context.add_variable("temp_rf_params", params)
+        else:
+            params = {
+                "C": trial.suggest_float("C", 0.001, 10.0, log=True),
+                "max_iter": trial.suggest_int("max_iter", 100, 2000)
+            }
+            context.add_variable("temp_lr_params", params)
+        
 
         try:
             PipelineBuilder.build_from_yaml("pipeline_config.yaml", orchestrator=orchestrator)
             orchestrator.run(context=context)
             logger.info("🏁 Pipeline ended successfuly")
-            return context.get_variable("last_accuracy")
+            return context.get_variable("last_accuracy", 0)
         except Exception as e:
             logger.error(f"❌ **PIPELINE CRITICAL FAIL** : {str(e)}")
-            raise
+            sys.exit(1)
 
     study = optuna.create_study(direction="maximize")
     study.optimize(objective, n_trials=20)
     return study.best_params
 
 @task(name="Version_And_Update_Config")
-def update_and_version_config(best_params):
+def update_and_version_config(rf_best, lr_best):
     # Versioning
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     version_name = f"pipeline_config_{timestamp}.yaml"
@@ -50,21 +57,23 @@ def update_and_version_config(best_params):
     with open("pipeline_config.yaml", "r") as f:
         config = yaml.safe_load(f)
     
-    # Dynamic RandomForest params update
-    for strategy in config['pipeline']['learning']['strategies']:
-        if "RandomForest" in strategy:
-            # Inject new params
-            config['pipeline']['params']['rf_params'] = best_params
+    if "learning" in config["pipeline"]:
+        config["pipeline"]["learning"]["params"]["rf_params"].update(rf_best)
+        config["pipeline"]["learning"]["params"]["lr_params"].update(lr_best)
             
     with open("pipeline_config.yaml", "w") as f:
-        yaml.dump(config, f)
+        yaml.dump(config, f, default_flow_style=False)
 
-    return version_name
+    logger.info(f"✅ pipeline_config.yaml was updated with Optuna best runs")
 
 @flow(name="Smart_Retraining")
 def training_flow():
-    best_params = optimize_hyperparams()
+    best_rf = optimize_hyperparams(algo="RF")
+    best_lr = optimize_hyperparams(algo="LR")
 
-    new_version = update_and_version_config(best_params=best_params)
+    update_and_version_config(rf_best=best_rf, lr_best=best_lr)
 
-    print(f"✅ Versioned and updated configuration: {new_version}")
+    logger.info("🚀 Final retrain with optimized parameters")
+    final_orch = PipelineOrchestrator()
+    PipelineBuilder.build_from_yaml("pipeline_config.yaml", final_orch)
+    final_orch.run(PipelineContext())
